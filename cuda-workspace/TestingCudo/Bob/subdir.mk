e84e42d56e0623ae00bae12d0592b3f90e74440f
Matt on Crackin <millermj@unk.edu>
2017-05-02 17:15:53 -0500

    Adding Cuda Code

---
+++ cuda-workspace/TestingCudo/Bob/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../test2.cu 
+
+OBJS += \
+./test2.o 
+
+CU_DEPS += \
+./test2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-8.0/bin/nvcc -G -g -O0 -std=c++11 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-8.0/bin/nvcc -G -g -O0 -std=c++11 --compile --relocatable-device-code=false -gencode arch=compute_50,code=compute_50 -gencode arch=compute_52,code=compute_52 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ cuda-workspace/TestingCudo/test2.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+
+typedef struct graph{
+
+	char * data;
+	char direction;
+}graph;
+
+__device__ graph * graphAllocate()
+{
+	graph* g = (graph *) malloc(sizeof(graph));
+	g->data = (char *)malloc(1000);
+	for(int i = 0; i < 1000;i++)
+	{
+		g->data[i] = 254;
+	}
+	g->direction = 'M';
+	printf("Before '%c' ", g->direction);
+	return g;
+}
+__device__ void b(int idx)
+{
+	graph * g = graphAllocate();
+	int sum = 1;
+	for(int i = 0; i < 1000;i++)
+	{
+		sum = (sum + g->data[i]) % idx;
+	}
+	printf("After'%c' %02d\n", g->direction,sum);
+	//printf("Device %d\n", idx);
+}
+
+__global__ void a()
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if(idx < N * N)
+	{
+		b(idx);
+	}
+}
+/*
+int main()
+{
+	//cout << "Yo Yo" << endl;
+	printf("Hii\n");
+	a<<<N,N>>>();
+	cudaDeviceSynchronize();
+	printf("Yo\n");
+	return 0;
+}*/
