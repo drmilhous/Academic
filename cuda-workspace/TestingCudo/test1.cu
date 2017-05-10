6bdf057dffccd16f5c4b138a21fb520e945349a3
Matt Miller <millermj@unk.edu>
2017-05-10 07:14:32 -0500

    auto push

--- cuda-workspace/TestingCudo/test1.cu
+++ cuda-workspace/TestingCudo/test1.cu
@@ -594,7 +594,7 @@ int foo(path * p)
 
 						for(int j = 0; j <MAX * 3; j+=3)
 						{
-						int idx = (row * size + col) * MAX +j;
+						int idx = (row * size + col) * MAX*3 +j;
 						if (result[idx]->ok == '1')
 							{
 								printf("(%d,%d,%d)\n", row, col, j);
