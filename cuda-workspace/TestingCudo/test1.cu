bedf60fdacbbaca7d559be7d0751a3f69813269d
Matt Miller <millermj@unk.edu>
2017-05-21 22:07:49 -0500

    auto push

--- cuda-workspace/TestingCudo/test1.cu
+++ cuda-workspace/TestingCudo/test1.cu
@@ -140,7 +140,7 @@ __device__ void computeIterative(grid * g, path * p, location * loc)
 			if(checkValue == 0 && count == MAX)
 			{
 				i ++;
-				//printGrid(currentGrid);
+				printGrid(currentGrid);
 			}
 			if (checkValue == 0 && count < MAX) //rec value
 				{
