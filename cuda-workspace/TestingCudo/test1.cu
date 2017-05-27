887be216e8836bb263bfa662a71e7d85fe8a993e
Matt Miller <millermj@unk.edu>
2017-05-27 17:11:24 -0500

    auto push

--- cuda-workspace/TestingCudo/test1.cu
+++ cuda-workspace/TestingCudo/test1.cu
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "grid.h"
-#define MAX 9
+#define MAX 18
 #define N 10
 void initCell(cell * c);
 __global__ void compute2(returnResult * res, grid * g, path ** pathList, location * l);
@@ -399,7 +399,7 @@ int foo(path ** p)
 		//for(int gridSize = 1000; gridSize < 1057; gridSize++)
 		{
 			
-			int breaker = 900000;
+			int breaker = 1000000;
 			printf("Starting %d\n", breaker);
 			res->result = result;
 			res->breaker = breaker;
