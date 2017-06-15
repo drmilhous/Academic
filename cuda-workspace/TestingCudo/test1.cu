4105aa15898869b073cccc3151a484fc1e0b0934
Matt Miller <millermj@unk.edu>
2017-06-15 10:55:52 -0500

    test divide

--- cuda-workspace/TestingCudo/test1.cu
+++ cuda-workspace/TestingCudo/test1.cu
@@ -169,7 +169,7 @@ gridResult * getGrids(path ** p, int MAX, int size)
 	larray[0].y = 0;
 	larray[0].full = FULL;
 	res->threads = 1;
-	int gridSize = 1000;
+	int gridSize = 10000;
 	int amount = gridSize * sizeof(grid *);
 	//printf("Allocated Bytes %d\n", amount);
 	cudaMallocManaged((void **) &result, amount);
