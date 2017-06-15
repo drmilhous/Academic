46575be7c8a8d690ee00f64db0522e12747a4fd9
Matt Miller <millermj@unk.edu>
2017-06-15 16:29:59 -0500

    change threads to 256

--- cuda-workspace/TestingCudo/test1.cu
+++ cuda-workspace/TestingCudo/test1.cu
@@ -102,7 +102,7 @@ void processGrids(gridResult * grids, path ** p,int MAX, int size)
 		larray[i].full = PART;
 	}
 	res->threads = grids->size;
-	int base = 512;
+	int base = 256;
 	int blocks = (grids->size / base);
 	if((grids->size % base) != 0)
 	{
