b6eb61abad5b61d25f5bf7f2a1368ab5f370a41c
Matt Miller <millermj@unk.edu>
2017-06-26 19:26:31 -0500

    testing

--- cuda-workspace/TestingCudo/FastGridMain.c
+++ cuda-workspace/TestingCudo/FastGridMain.c
@@ -58,8 +58,8 @@ int main(int argc, char ** argv)
 		int depth = 3;
 		State * stateStack = allocateStateStack(threads, depth, N); 
 		initThreads(stateStack, threads, depth,N, path);
-		stackState[0].location.x = 1;
-		stackState[0].location.y = 2;
+		stateStack[0].location.x = 1;
+		stateStack[0].location.y = 2;
 		compute<<<blocks, threadBlocks>>>(g,N ,threads, stateStack, depth);
 		cudaDeviceSynchronize();
 		//printGrid(g,N);
