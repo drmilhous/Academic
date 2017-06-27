1e271b5e196d043b90e7914d6d494d475350a38a
Matt Miller <millermj@unk.edu>
2017-06-26 21:26:18 -0500

    testing

--- cuda-workspace/TestingCudo/FastGridMain.c
+++ cuda-workspace/TestingCudo/FastGridMain.c
@@ -82,7 +82,7 @@ void initThreads(State * s, int threads, int depth, int N, Path ** path)
 	{
 		base = path;
 		current = path[0];
-		t++;
+		current++;
 		for(int d = 0; d < depth-1; d++)
 		{
 			if(current == NULL || current->next == NULL) 
@@ -146,17 +146,13 @@ __device__ void computeLocal(State * s,int N, int depth, int max)
 {
 	int value;
 	initLocation(&s[depth]);
-	depth++;
-	s[depth].path = s[depth-1].path;
 	int count = 0;
 	int maxCount = 10;
-
-
 
 	for(int i = 0; i < N; i++)
 	{
 		printf("depth[%d] x[%d] y[%d] nx[%d] ny[%d]\n", depth,s[depth].location.x, s[depth].location.y, s[depth].location.nextX, s[depth].location.nextY );
-		cloneState(s[depth-1], s[depth],N);
+		//cloneState(s[depth-1], s[depth],N);
 		value = setAll(&s[depth].grid, s[depth].path, &s[depth].location, N);
 		printf("Before\n");
 		printGridDev(&s[depth-1].grid, N);
