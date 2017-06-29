5929535b64478a83dcf97853b5e71115ad0ebcd1
Matt Miller <millermj@unk.edu>
2017-06-29 07:31:49 -0500

    testing

--- cuda-workspace/TestingCudo/FastGridMain.c
+++ cuda-workspace/TestingCudo/FastGridMain.c
@@ -152,7 +152,7 @@ __device__ void computeLocal(State * s,int N, int depth, int max)
 	int count = 0;
 	int maxCount = 10;
 
-	for(int i = 0; i < 100; i++)
+	for(int i = 0; i < 30; i++)
 	{
 		int pop = 0;
 		hasNext = 0;
