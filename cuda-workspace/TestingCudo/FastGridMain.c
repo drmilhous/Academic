df00f1ab6cd9a60b207d58bbded7fb4d427eaf0b
Matt Miller <millermj@unk.edu>
2017-06-30 09:55:00 -0500

    full

--- cuda-workspace/TestingCudo/FastGridMain.c
+++ cuda-workspace/TestingCudo/FastGridMain.c
@@ -153,6 +153,7 @@ __device__ void computeLocal(State * s,int N, int depth, int max)
 	int value;
 	int hasNext = 0;
 	depth++;
+	cloneState(s[depth-1], s[depth],N);
 	initLocation(&s[depth]);
 	int count = 0;
 	int maxCount = 10;
@@ -163,7 +164,7 @@ __device__ void computeLocal(State * s,int N, int depth, int max)
 		s[depth].iterations++;
 		pop = 0;
 		hasNext = 0;
-		cloneState(s[depth-1], s[depth],N);
+		//cloneState(s[depth-1], s[depth],N);
 		value = setAll(&s[depth].grid, s[depth].path, &s[depth].location, N);
 		printf("depth[%d] x[%d] y[%d] nx[%d] ny[%d] value(%d)\n", depth,s[depth].location.x, s[depth].location.y, s[depth].location.nextX, s[depth].location.nextY , value);
 		//printf("Before\n");
