01ea2c4f116cdf225a5f8e3f2664e84f911bbae1
Matt Miller <millermj@unk.edu>
2017-07-08 23:40:55 -0500

    print time

--- cuda-workspace/TestingCudo/FastGridMain.c
+++ cuda-workspace/TestingCudo/FastGridMain.c
@@ -163,15 +163,15 @@ void computeFull(StateList * initState,Path ** path, int N,int depth, int thread
 		double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
 		//printf("Time spent %lf\n", time_spent);
 		printf("Time spent %lf\n", time_spent);
-		printf("seconds %lf ", time_spent %60);
+		printf("seconds %lf ", time_spent %60l);
 		if(time_spent > 60)
 		{
 			time_spent /= 60;
-			printf("minutes %lf ", time_spent%60);
+			printf("minutes %lf ", time_spent%60l);
 			if(time_spent > 60)
 			{
 				time_spent /= 60;
-				printf("hours %lf ", time_spent%24);
+				printf("hours %lf ", time_spent%24l);
 				if(time_spent > 24)
 				{
 					time_spent /= 24;
